e4de1d4e8357d3e7fc8db13e183ac6ecb57bc0e2
ohanz <chiaguguadaniel@gmail.com>
2024-09-01 04:12:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hye there, It's Ohanz!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ohanz Just Edited <code>src/App.js</code> and saved to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="javascript:void(0)"
+          // target="_blank"
+          // rel="noopener noreferrer"
         >
-          Learn React
+          Visit Ihype
         </a>
       </header>
     </div>
